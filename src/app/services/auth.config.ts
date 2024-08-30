a62922b479e4e8ac617b22b058c1bbd2e7c469ad
David Ivanovic <david.ivanovic@serengetitech.com>
2024-08-30 13:21:52 +0200

    initial commit

--- src/app/services/auth.config.ts
+++ src/app/services/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
   redirectUri: window.location.origin,
-  clientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
+  clientId: '3534841119-7jps5d3s9e6ges9rpc8hqiqjt5id5n33.apps.googleusercontent.com',
   scope: 'openid profile email',
   strictDiscoveryDocumentValidation: false,
 };
